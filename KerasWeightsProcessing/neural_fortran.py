76c913027c48e35f3b654c73f92e6809442f0aba
Jordan Ott <jordanott365@gmail.com>
2019-10-08 15:01:53 -0700

    Separate activations

--- KerasWeightsProcessing/neural_fortran.py
+++ KerasWeightsProcessing/neural_fortran.py
@@ -3,6 +3,8 @@ import json
 import argparse
 import numpy as np
 
+ACTIVATIONS = ['ReLU', 'Linear', 'LeakyReLU']
+
 def get_available_layers(layers, available_layers=[b"dense"]):
     parsed_layers = []
     for l in layers:
@@ -16,7 +18,8 @@ def h5_to_txt(weights_file_name, output_file_name=''):
     #check and open file
     with h5py.File(weights_file_name,'r') as weights_file:
 
-        weights_group_key=list(weights_file.keys())[0]
+        # 'model_weights'=list(weights_file.keys())[0]
+        model_weights = weights_file['model_weights']
 
         # activation function information in model_config
         model_config = weights_file.attrs['model_config'].decode('utf-8') # Decode using the utf-8 encoding
@@ -26,18 +29,15 @@ def h5_to_txt(weights_file_name, output_file_name=''):
         model_config = model_config.replace('null','None')
         model_config = eval(model_config)
 
-        layers = list(weights_file['model_weights'].attrs['layer_names'])
+        layers = list(model_weights.attrs['layer_names'])
         layers = get_available_layers(layers)
         print("names of layers in h5 file: %s \n" % layers)
 
-        # attributes needed for .txt file
-        # number of layers + 1(Fortran includes input layer),
-        #   dimensions, biases, weights, and activations
         num_layers = len(layers)+1
 
-        dimensions = []
-        bias = {}
-        weights = {}
+        bias        = []
+        weights     = []
+        dimensions  = []
         activations = []
 
         print('Processing the following {} layers: \n{}\n'.format(len(layers),layers))
@@ -46,26 +46,36 @@ def h5_to_txt(weights_file_name, output_file_name=''):
         else:
             model_config = model_config['config']['layers']
 
-        for num,l in enumerate(layers):
-            layer_info_keys=list(weights_file[weights_group_key][l][l].keys())
+        for idx,layer in enumerate(model_config):
+            name       = layer['name']
+            class_name = layer['class_name']
 
-            #layer_info_keys should have 'bias:0' and 'kernel:0'
+            print('Layer name:', name)
+            try:
+                layer_info_keys = list(model_weights[name][name].keys())
+            except:
+                layer_info_keys = []
+
+            print(layer_info_keys)
             for key in layer_info_keys:
                 if "bias" in key:
-                    bias.update({num:np.array(weights_file[weights_group_key][l][l][key])})
+                    bias.append(np.array(model_weights[name][name][key]))
 
                 elif "kernel" in key:
-                    weights.update({num:np.array(weights_file[weights_group_key][l][l][key])})
-                    if num == 0:
-                        dimensions.append(str(np.array(weights_file[weights_group_key][l][l][key]).shape[0]))
-                        dimensions.append(str(np.array(weights_file[weights_group_key][l][l][key]).shape[1]))
+                    weights.append(np.array(model_weights[name][name][key]))
+                    if idx == 0:
+                        dimensions.append(str(np.array(model_weights[name][name][key]).shape[0]))
+                        dimensions.append(str(np.array(model_weights[name][name][key]).shape[1]))
                     else:
-                        dimensions.append(str(np.array(weights_file[weights_group_key][l][l][key]).shape[1]))
+                        dimensions.append(str(np.array(model_weights[name][name][key]).shape[1]))
 
-            if 'Dense' in model_config[num]['class_name']:
-                activations.append(model_config[num]['config']['activation'])
-            else:
-                print('Skipping bad layer: \'{}\'\n'.format(model_config[num]['class_name']))
+            if 'Dense' in class_name:
+                activation = layer['config']['activation']
+                activations.append(activation)
+
+            elif class_name in ACTIVATIONS:
+                activations[-1] = class_name.lower()
+                print('Skipping bad layer: \'{}\'\n'.format(class_name.lower()))
 
     if not output_file_name:
         # if not specified will use path of weights_file with txt extension
@@ -76,12 +86,12 @@ def h5_to_txt(weights_file_name, output_file_name=''):
 
         output_file.write("\t".join(dimensions) + '\n')
         if bias:
-            for x in range(len(layers)):
-                bias_str="\t".join(list(map(str,bias[x].tolist())))
+            for b in bias:
+                bias_str="\t".join(list(map(str,b.tolist())))
                 output_file.write(bias_str + '\n')
         if weights:
-            for x in range(len(layers)):
-                weights_str="\t".join(list(map(str,weights[x].T.flatten())))
+            for w in weights:
+                weights_str="\t".join(list(map(str,w.T.flatten())))
                 output_file.write(weights_str + '\n')
         if activations:
             for a in activations:
