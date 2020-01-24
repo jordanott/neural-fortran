aebb315d147b74f27495e946c5adaee19078c38d
Jordan Ott <jordanott365@gmail.com>
2020-01-23 18:25:17 -0800

    Simple example test

--- src/tests/test_keras.F90
+++ src/tests/test_keras.F90
@@ -1,5 +1,8 @@
 ! test_keras.f90
 
+! TO RUN
+! ./test_keras $NF_PATH/ExampleModels/simple_model.txt
+
 ! read in some data
 ! read in some network from .txt(?) file
 !   network will have been trained in keras
@@ -23,7 +26,7 @@ program test_keras
   ! load trained network from keras
   call net % load(args(1))
 
-  input = [1, 2, 3, 4, 5]
+  input = [10, 2, 3, 4, 5]
 
   ! run test input through network
   result1 = net % output(input)
